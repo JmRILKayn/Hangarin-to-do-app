8211ced9480ce6757f477bd0a2a27f686dd07149
JM MAYORES <josemiguelmayores@gmail.com>
2026-03-21 23:01:16 +0800

    changes

--- hangarin/views.py
+++ hangarin/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Task
 
 def home(request):
     return render(request, 'hangarin/home.html')
+
+def dashboard(request):
+    tasks = Task.objects.all().order_by('deadline')
+    return render(request, 'hangarin/dashboard.html', {'tasks': tasks})
--- projectsite/urls.py
+++ projectsite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hangarin.views import home  
+from hangarin.views import home, dashboard 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', home, name='home'), 
+    path('dashboard/', dashboard, name='dashboard'), 
 ]
