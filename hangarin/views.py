767cef15844066fa7c270279cb2921437723b327
JM MAYORES <josemiguelmayores@gmail.com>
2026-03-22 00:03:42 +0800

    changes

--- hangarin/views.py
+++ hangarin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def home(request):
@@ -7,3 +7,14 @@ def home(request):
 def dashboard(request):
     tasks = Task.objects.all().order_by('deadline')
     return render(request, 'hangarin/dashboard.html', {'tasks': tasks})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.status = 'Completed'
+    task.save()
+    return redirect('dashboard')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('dashboard')
--- projectsite/urls.py
+++ projectsite/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hangarin.views import home, dashboard 
+from. import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'), 
     path('dashboard/', dashboard, name='dashboard'), 
+    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
